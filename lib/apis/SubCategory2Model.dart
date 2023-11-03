59e8fcc9fcee2d9c0d847ac7cc887fb299ede600
Saksham gupta <guptasaksham3071gmail.com>
2023-11-03 16:14:24 +0530

    Category dialog Fix & Login update

--- lib/apis/SubCategory2Model.dart
+++ lib/apis/SubCategory2Model.dart
@@ -1,17 +1,20 @@
 class SubCategory2Model {
-  final int status;
-  final List<String> data;
+  String msg;
+  List<String> data;
 
-  SubCategory2Model({required this.status, required this.data});
+  SubCategory2Model({required this.msg, required this.data});
 
   factory SubCategory2Model.fromJson(Map<String, dynamic> json) {
-    List<String> subCategories = (json['data'] as List)
-        .map((item) => item.toString())
-        .toList();
-
     return SubCategory2Model(
-      status: json['status'],
-      data: subCategories,
+      msg: json['msg'],
+      data: List<String>.from(json['data']),
     );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'msg': msg,
+      'data': data,
+    };
   }
 }
--- lib/apis/SubCategoryModel.dart
+++ lib/apis/SubCategoryModel.dart
@@ -1,33 +1,20 @@
 class SubCategoryModel {
-  final int status;
-  final List<SubCategory> data;
+  String msg;
+  List<String> data;
 
-  SubCategoryModel({required this.status, required this.data});
+  SubCategoryModel({required this.msg, required this.data});
 
   factory SubCategoryModel.fromJson(Map<String, dynamic> json) {
-    List<SubCategory> subCategoryList = (json['data'] as List)
-        .map((subCategory) => SubCategory.fromJson(subCategory))
-        .toList();
-
     return SubCategoryModel(
-      status: json['status'],
-      data: subCategoryList,
+      msg: json['msg'],
+      data: List<String>.from(json['data']),
     );
   }
-}
 
-class SubCategory {
-  final String id;
-  final List<String> subCategory1;
-
-  SubCategory({required this.id, required this.subCategory1});
-
-  factory SubCategory.fromJson(Map<String, dynamic> json) {
-    return SubCategory(
-      id: json['_id'],
-      subCategory1: (json['subCategory1'] as List)
-          .map((item) => item.toString())
-          .toList(),
-    );
+  Map<String, dynamic> toJson() {
+    return {
+      'msg': msg,
+      'data': data,
+    };
   }
 }
